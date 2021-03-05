ff8dfde92a3d45d37881f24456b9f0a6f6921034
Amol44 <39632426+Amol44@users.noreply.github.com>
2021-03-05 09:01:41 +0100

    First

--- subjects/ex03-divideandconquer.js
+++ subjects/ex03-divideandconquer.js
@@ -1,11 +1,17 @@
 // Given a mixed array of number and string representations of integers, add up the string integers and subtract this from the total of the non-string integers. 
 
 function divideAndConquer( inputArray ) {
-  // AJOUTER VOTRE CODE ICI ...
-  let num1 = 0,
-      num2 = 0;
-  x.map(v => (typeof v === 'string' ? (num2 += v * 1) : (num1 -= v)));
-  return num1 - num2;
+  let rcResult = 0;
+
+  for (let value of inputArray) {
+    if (typeof(value) === 'string') {
+      rcResult -= parseInt(value);
+    } else {
+      rcResult += value;
+    }
+  }
+  
+  return rcResult;
 }
 
 ///////////////////////////////////////
--- subjects/ex05-recycle.js
+++ subjects/ex05-recycle.js
@@ -9,79 +9,107 @@
 //
 
 function recycle(inputArray) {
-  // AJOUTER VOTRE CODE ICI ...
-  let paper=0, glass=0, organic=0, plastic=0;
-  recycle.map(v=>v===0?paper++:v<0?glass++:organic++:plastic++)
-  return [paper, glass, organic, plastic]
+    let paperArray = [], glassArray = [], organicArray = [], plasticArray = [];
+  
+    inputArray.forEach(rubbish => {  
+      if(rubbish.material === 'paper' || rubbish.secondMaterial === 'paper' ) paperArray.push(rubbish.type)
+      if(rubbish.material === 'glass' || rubbish.secondMaterial === 'glass') glassArray.push(rubbish.type)
+      if(rubbish.material === 'organic' || rubbish.secondMaterial === 'organic') organicArray.push(rubbish.type)
+      if(rubbish.material === 'plastic' || rubbish.secondMaterial === 'plastic') plasticArray.push(rubbish.type)
+    });
+  
+    return [paperArray, glassArray, organicArray, plasticArray];
 }
 
 ///////////////////////////////////////
 //// TEST CODE - DO NOT TOUCH !!!  ////
 ///////////////////////////////////////
 function testRecycle() {
-  const iconTestSuccess = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill icon-green" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"></path></svg>';
-  const iconTestFailed  = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-circle-fill icon-red" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/></svg>';
+    const iconTestSuccess = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill icon-green" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"></path></svg>';
+    const iconTestFailed  = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-circle-fill icon-red" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/></svg>';
+  
+    const textTestSuccess = 'TEST SUCCESS - Well done !\n';
+    const textTestFailed  = 'TEST FAILED - Keep going !\n';
+    const textCountSuccess = 'Passed : ';
+    const textCountFailed = ' Failed : ';
+  
+    // Jeu de données de test par couple [ Oracle, Résultat attendu ]
+    const testOracle = 0;
+    const testExpected = 1;
+    const oracleT1 = [
+        {type: 'rotten apples', material: 'organic'},
+        {type: 'out of date yogurt', material: 'organic', secondMaterial: 'plastic'},
+        {type: 'wine bottle', material: 'glass', secondMaterial: 'paper'},
+        {type: 'amazon box', material: 'paper'},
+        {type: 'beer bottle', material: 'glass', secondMaterial: 'paper'}
+      ];
+    const expT1 = [
+        ['wine bottle', 'amazon box', 'beer bottle'],   // paper
+        ['wine bottle', 'beer bottle'],                 // glass
+        ['rotten apples', 'out of date yogurt'],        // organic
+        ['out of date yogurt']                          // plastic
+      ];
+    
+    const oracleT2 = [
+        {type: 'pin apples', material: 'organic'},
+        {type: 'orange juice', material: 'glass'},
+        {type: 'out of date milk', material: 'organic', secondMaterial: 'paper'},
+        {type: 'newspaper', material: 'paper'},
+        {type: 'cornflakes box', material: 'plastic', secondMaterial: 'paper'}
+      ];
+    const expT2 = [
+        ['out of date milk', 'newspaper', 'cornflakes box'],
+        ['orange juice'],
+        ['pin apples', 'out of date milk'],
+        ['cornflakes box']
+      ];
 
-  const textTestSuccess = 'TEST SUCCESS - Well done !\n';
-  const textTestFailed  = 'TEST FAILED - Keep going !\n';
-  const textCountSuccess = 'Passed : ';
-  const textCountFailed = ' Failed : ';
-
-  // Jeu de données de test par couple [ Oracle, Résultat attendu ]
-  const testOracle = 0;
-  const testExpected = 1;
-  const oracleT1 = [
-      {type: 'rotten apples', material: 'organic'},
-      {type: 'out of date yogurt', material: 'organic', secondMaterial: 'plastic'},
-      {type: 'wine bottle', material: 'glass', secondMaterial: 'paper'},
-      {type: 'amazon box', material: 'paper'},
-      {type: 'beer bottle', material: 'glass', secondMaterial: 'paper'}
-    ];
-  const expT1 = [
-      ['wine bottle', 'amazon box', 'beer bottle'],   // paper
-      ['wine bottle', 'beer bottle'],                 // glass
-      ['rotten apples', 'out of date yogurt'],        // organic
-      ['out of date yogurt']                          // plastic
+    const testDataSet = [
+      [oracleT1, expT1],
+      [oracleT2, expT2]
     ];
   
-  const oracleT2 = [
-      {type: 'pin apples', material: 'organic'},
-      {type: 'orange juice', material: 'glass'},
-      {type: 'out of date milk', material: 'organic', secondMaterial: 'paper'},
-      {type: 'newspaper', material: 'paper'},
-      {type: 'cornflakes box', material: 'plastic', secondMaterial: 'paper'}
-    ];
-  const expT2 = [
-      ['out of date milk', 'newspaper', 'cornflakes box'],
-      ['orange juice'],
-      ['pin apples', 'out of date milk'],
-      ['cornflakes box']
-    ];
-
-  const testDataSet = [
-    [oracleT1, expT1],
-    [oracleT2, expT2]
-  ];
-
-  let rcResult = "";
-  let rcSuccess = 0;
-  let rcFailed = 0;
-  let rcTotal = 0;
-  for (let testData of testDataSet ) {
-    rcTotal++;
-    if (JSON.stringify(recycle(testData[testOracle])) === JSON.stringify(testData[testExpected])) {
-      rcResult += iconTestSuccess + " #0" + rcTotal + " " + textTestSuccess;
-      rcSuccess++;
-    } else {
-      rcResult += iconTestFailed + " #0" + rcTotal + " " + textTestFailed;
-      rcFailed++;
+    let rcResult = "";
+    let rcSuccess = 0;
+    let rcFailed = 0;
+    let rcTotal = 0;
+    for (let testData of testDataSet ) {
+      rcTotal++;
+      if (JSON.stringify(recycle(testData[testOracle])) === JSON.stringify(testData[testExpected])) {
+        rcResult += iconTestSuccess + " #0" + rcTotal + " " + textTestSuccess;
+        rcSuccess++;
+      } else {
+        rcResult += iconTestFailed + " #0" + rcTotal + " " + textTestFailed;
+        rcFailed++;
+      }
     }
+    rcResult = textCountSuccess + rcSuccess + textCountFailed + rcFailed + '\n' + rcResult;
+  
+    return rcResult;
   }
-  rcResult = textCountSuccess + rcSuccess + textCountFailed + rcFailed + '\n' + rcResult;
+  
+  document.getElementById('ex05-test-rc').innerHTML = testRecycle();
+  
+  ///////////////// EOF /////////////////
+  
 
-  return rcResult;
+/* ALTERNATE SOLUTION
+function recycle(inputArray) {
+    let materials = { "paper": [], "glass": [], "organic": [], "plastic": [] }
+  
+    inputArray.forEach(o => {
+      materials[o.material].push(o.type);
+      if(o.secondMaterial)
+        materials[o.secondMaterial].push(o.type);
+    });
+    return Object.values(materials);
 }
-  
-document.getElementById('ex05-test-rc').innerHTML = testRecycle();
-  
-///////////////// EOF /////////////////
+*/
+
+/* ALTERNATIVE SOLUTION
+const recyclerMaterials = ['paper', 'glass', 'organic', 'plastic'];
+
+function recycle(objects) {
+  return recyclerMaterials.map(m => objects.filter(o => o.material == m || o.secondMaterial == m).map(o => o.type));
+}
+*/
